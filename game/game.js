d393e3ade7473ae3c7a8ec2e0454133bcc6636a7
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-09-14 13:56:13 +0200

    Edited image in game

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update, render: render});
 function preload() {
-    game.load.image('obrazek', 'obrazek.jpg');
+    game.load.image('obrazek', 'obrazek.png');
 }
 function create() {
     game.add.image(100, 100, 'obrazek');
