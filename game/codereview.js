20830fbf4e13ffc5ab5857101908eacbc5786233
Łukasz Basaj <lbasaj@gmail.com>
2017-09-17 13:36:48 +0200

    fizzbuzz

--- game/codereview.js
+++ game/codereview.js
@@ -1,14 +1,12 @@
-var i;
-for (i = 1; i <= 100; i++) {
-    var m3 = i % 3 === 0;
-    var m5 = i % 5 === 0;
-    if (m3 && m5) {
-        console.log('FizzBuzz');
-    } else if (m3) {
-        console.log('Fizz');
-    } else if (m5) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
+for(var i = 1; 1<= 100; i++)
+{
+    if( i % 3 === 0 &&i%5===0) {
+        console.log('FooBaz');
+    }else if(i % 3 === 0) {
+        console.log('Foo');
+    }else if(i % 5 === 0) {
+        console.log('Baz')
+    }else{
+        console.log(i)
     }
 }
