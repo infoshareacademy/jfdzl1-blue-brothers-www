852a8472ef9f649fc00241bd492af762373c5fb8
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-09-17 13:48:35 +0200

    edited code

--- code.js
+++ code.js
@@ -3,13 +3,12 @@ for (var i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0)
         console.log("FuzzBuzz");
 
-    else
-        if (i % 5 == 0)
-            console.log("Buzz");
-        else if (i % 15 == 0)
-            console.log("Fuzz");
-        else
-            console.log(i);
-
-
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else if (i % 15 == 0) {
+        console.log("Fuzz");
+    } else {
+        console.log(i);
     }
+
+}
